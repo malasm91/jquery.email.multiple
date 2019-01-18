a74c61dda39207efe0795524ad026835d63f936c
Malal91 <controlmalal@gmail.com>
2019-01-18 09:21:31 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# jquery.email.multiple
---
+++ jquery.email.multiple.js
@@ -0,0 +1,70 @@
+/**
+ * Created by Malal91 and Haziel
+ * Select multiple email by jquery.email_multiple
+ * **/
+
+(function($){
+
+    $.fn.email_multiple = function(options) {
+
+        let defaults = {
+            reset: false,
+            fill: false,
+            data: null
+        };
+
+        let settings = $.extend(defaults, options);
+        let email = "";
+
+        return this.each(function()
+        {
+            $(this).after("<span class=\"to-input\">Email :</span>\n" +
+                "<div class=\"all-mail\"></div>\n" +
+                "<input type=\"text\" name=\"email\" class=\"enter-mail-id\" placeholder=\"Entrer l'email ...\" />");
+            let $orig = $(this);
+            let $element = $('.enter-mail-id');
+            $element.keydown(function (e) {
+                $element.css('border', '');
+                if (e.keyCode === 13 || e.keyCode === 32) {
+                    let getValue = $element.val();
+                    if (/^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,6}$/.test(getValue)){
+                        $('.all-mail').append('<span class="email-ids">' + getValue + '<span class="cancel-email">x</span></span>');
+                        $element.val('');
+
+                        email += getValue + ';'
+                    } else {
+                        $element.css('border', '1px solid red')
+                    }
+                }
+
+                $orig.val(email.slice(0, -1))
+            });
+
+            $(document).on('click','.cancel-email',function(){
+                $(this).parent().remove();
+            });
+
+            if(settings.data){
+                $.each(settings.data, function (x, y) {
+                    if (/^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,6}$/.test(y)){
+                        $('.all-mail').append('<span class="email-ids">' + y + '<span class="cancel-email">x</span></span>');
+                        $element.val('');
+
+                        email += y + ';'
+                    } else {
+                        $element.css('border', '1px solid red')
+                    }
+                })
+
+                $orig.val(email.slice(0, -1))
+            }
+
+            if(settings.reset){
+                $('.email-ids').remove()
+            }
+
+            return $orig.hide()
+        });
+    };
+
+})(jQuery);
